6a690e5a43a3b0822832fab7959d900adcc72955
Harshvardhan Shete <harshvardhan@logibricks.in>
2022-05-30 00:24:18 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import axios from "axios";
+
+import "./App.css";
 
 function App() {
+  const [data, setData] = React.useState([]);
+  React.useEffect(() => {
+    axios.get("https://cafe-peter-mern.herokuapp.com/read").then((res) => {
+      setData(res.data);
+    });
+  }, []);
+  console.log("data", data);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        flexDirection:'column'
+      }}
+    >
+      {data?.map((user, index) => {
+        return (
+          <div slyle={{ display: "flex", flexDirection:'column' }}>
+            <table
+              style={{
+                borderCollapse: "collapse",
+                width: "90%",
+                border: "1px solid #ccc",
+              }}
+            >
+              <tr>
+                <td id="left" style={{ width: "30%" }}>
+                  Serial No.
+                </td>
+                <td id="left" style={{ width: "30%" }}>
+                  Name
+                </td>
+                <td id="center" style={{ width: "40%" }}>
+                  Email
+                </td>
+                <td id="right" style={{ width: "30%" }}>
+                  Phone
+                </td>
+              </tr>
+              <tr>
+                <td>{user?.index}</td>
+                <td>{user?.userName}</td>
+                <td>{user?.email}</td>
+                <td>{user?.phone}</td>
+              </tr>
+            </table>
+            
+          </div>
+        );
+      })}
     </div>
+    </>
+
   );
 }
 
